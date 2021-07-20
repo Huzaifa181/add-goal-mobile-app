8835cf89fd07686dff2dad2d6a0721a67103016b
Huzaifa181 <huzaifa.ahmed181@gmail.com>
2021-07-21 00:13:31 +0500

    Add item into the list

--- App.js
+++ App.js
@@ -7,6 +7,8 @@ import {
   TextInput,
   useColorScheme,
   View,
+  FlatList,
+  Text,
   Button,
 } from 'react-native';
 import {
@@ -18,22 +20,50 @@ import {
 
 const App = () => {
   const isDarkMode = useColorScheme() === 'dark';
-
+  const [text, setText] = useState('');
+  const [courseGoals, setCourseGoals] = useState([]);
+  const addGoalHandler = () => {
+    setCourseGoals([...courseGoals, {key: Math.random().toString(), text}]);
+    setText('');
+  };
   return (
     <View padding={30}>
-      <View>
+      <View style={{flexDirection: 'row', alignItems: 'center'}}>
         <TextInput
           placeholder="Course Goal"
+          onChangeText={setText}
+          value={text}
           style={{
+            flex: 1,
             borderBottomColor: 'black',
             borderBottomWidth: 1,
           }}></TextInput>
-        <Button title="Add"></Button>
+        <Button title="Add" onPress={addGoalHandler}></Button>
+      </View>
+      <View>
+        <FlatList
+          data={courseGoals}
+          keyExtractor={(item, index) => item.id}
+          renderItem={itemData => {
+            return (
+              <View style={styles.listItem}>
+                <Text>{itemData.item}</Text>
+              </View>
+            );
+          }}></FlatList>
       </View>
     </View>
   );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  listItem: {
+    padding: 10,
+    marginVertical: 10,
+    backgroundColor: '#ccc',
+    borderColor: 'black',
+    borderWidth: 1,
+  },
+});
 
 export default App;
